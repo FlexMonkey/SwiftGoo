48c17897d42dd3319939cead9ba41d4cf26408e7
simon gladman <sgladman@gmail.com>
2016-04-13 08:01:17 -0700

    Working demo

---
+++ SwiftGoo/ImageView.swift
@@ -0,0 +1,117 @@
+//
+//  ImageView.swift
+//  SwiftGoo
+//
+//  Created by Simon Gladman on 13/04/2016.
+//  Copyright © 2016 Simon Gladman. All rights reserved.
+//
+
+import GLKit
+
+// ----------------
+
+/// `OpenGLImageView` wraps up a `GLKView` and its delegate into a single class to simplify the
+/// display of `CIImage`.
+///
+/// `OpenGLImageView` is hardcoded to simulate ScaleAspectFit: images are sized to retain their
+/// aspect ratio and fit within the available bounds.
+///
+/// `OpenGLImageView` also respects `backgroundColor` for opaque colors
+
+class OpenGLImageView: GLKView
+{
+    let eaglContext = EAGLContext(API: .OpenGLES2)
+    
+    lazy var ciContext: CIContext =
+    {
+        [unowned self] in
+        
+        return CIContext(EAGLContext: self.eaglContext,
+            options: [kCIContextWorkingColorSpace: NSNull()])
+    }()
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame, context: eaglContext)
+    
+        context = self.eaglContext
+        delegate = self
+    }
+
+    override init(frame: CGRect, context: EAGLContext)
+    {
+        fatalError("init(frame:, context:) has not been implemented")
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /// The image to display
+    var image: CIImage?
+    {
+        didSet
+        {
+            setNeedsDisplay()
+        }
+    }
+}
+
+extension OpenGLImageView: GLKViewDelegate
+{
+    func glkView(view: GLKView, drawInRect rect: CGRect)
+    {
+        guard let image = image else
+        {
+            return
+        }
+   
+        let targetRect = image.extent.aspectFitInRect(
+            target: CGRect(origin: CGPointZero,
+                size: CGSize(width: drawableWidth,
+                    height: drawableHeight)))
+        
+        let ciBackgroundColor = CIColor(
+            color: backgroundColor ?? UIColor.whiteColor())
+        
+        ciContext.drawImage(CIImage(color: ciBackgroundColor),
+            inRect: CGRect(x: 0,
+                y: 0,
+                width: drawableWidth,
+                height: drawableHeight),
+            fromRect: CGRect(x: 0,
+                y: 0,
+                width: drawableWidth,
+                height: drawableHeight))
+        
+        ciContext.drawImage(image,
+            inRect: targetRect,
+            fromRect: image.extent)
+    }
+}
+
+extension CGRect
+{
+    func aspectFitInRect(target target: CGRect) -> CGRect
+    {
+        let scale: CGFloat =
+        {
+            let scale = target.width / self.width
+            
+            return self.height * scale <= target.height ?
+                scale :
+                target.height / self.height
+        }()
+        
+        let width = self.width * scale
+        let height = self.height * scale
+        let x = target.midX - width / 2
+        let y = target.midY - height / 2
+        
+        return CGRect(x: x,
+            y: y,
+            width: width,
+            height: height)
+    }
+}
--- SwiftGoo/ViewController.swift
+++ SwiftGoo/ViewController.swift
@@ -8,18 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    let imageView = OpenGLImageView()
+    let mona = CIImage(image: UIImage(named: "monalisa.jpg")!)
+    
+    let accumulator = CIImageAccumulator(extent: CGRect(x: 0, y: 0, width: 640, height: 640),
+                                         format: kCIFormatARGB8)
+    
+    let warpKernel = CIWarpKernel(string:
+        "kernel vec2 warp(float radius, vec2 location, vec2 direction)" +
+        "{ " +
+        " float dist = distance(location, destCoord()); " +
+        
+        "  if (dist < radius)" +
+        "  { " +
+            
+        "     float normalisedDistance = 1.0 - (dist / radius); " +
+        "     float smoothedDistance = smoothstep(0.0, 1.0, normalisedDistance); " +
+            
+        "    return destCoord() + (direction * 0.2) * smoothedDistance; " +
+        "  } else { " +
+        "  return destCoord();" +
+        "  }" +
+        "}")!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        view.addSubview(imageView)
+        
+        accumulator.setImage(mona!)
+        
+        imageView.image = accumulator.image()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews()
+    {
+        imageView.frame = CGRect(x: 0, y: 0, width: 640, height: 640)
+        
+        imageView.frame.origin.x = view.frame.midX - imageView.frame.midX
+        imageView.frame.origin.y = view.frame.midY - imageView.frame.midY
     }
 
-
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        guard let touch = touches.first,
+            coalescedTouches = event?.coalescedTouchesForTouch(touch)
+            where imageView.bounds.contains(touch.locationInView(imageView)) else
+        {
+            return
+        }
+        
+        for coalescedTouch in [touch] // coalescedTouches
+        {
+            let locationInViewY = accumulator.image().extent.height - coalescedTouch.locationInView(imageView).y
+            
+            let location = CIVector(x: coalescedTouch.locationInView(imageView).x, y: locationInViewY)
+            let direction = CIVector(x: coalescedTouch.previousLocationInView(imageView).x - touch.locationInView(imageView).x,
+                                     y: coalescedTouch.locationInView(imageView).y - touch.previousLocationInView(imageView).y)
+            
+            let arguments = [CGFloat(100), location, direction]
+            
+            let image = warpKernel.applyWithExtent(accumulator.image().extent,
+                                                    roiCallback:
+                {
+                    (index, rect) in
+                    return rect
+                },
+                                                    inputImage: accumulator.image(),
+                                                    arguments: arguments)
+            
+            accumulator.setImage(image!)
+            
+            imageView.image = accumulator.image()
+        }
+    }
+    
+    
 }
 
