c2caad55ebf99899bc62fcd473463944bd4a0abb
simon gladman <sgladman@gmail.com>
2016-04-13 04:19:00 -0700

    Initial Commit

---
+++ SwiftGoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftGoo
+//
+//  Created by Simon Gladman on 13/04/2016.
+//  Copyright © 2016 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
